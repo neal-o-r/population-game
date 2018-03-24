5409ff605060dc63f29113a5554d4a7f5c50e704
neal-o-r <neal@n-o-r.xyz>
2018-03-24 22:21:34 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Population Game
+
+A population bracket game played with the 32 counties of Ireland.
+
---
+++ bracket.py
@@ -0,0 +1,60 @@
+import random
+from collections import namedtuple
+import os
+
+def get_pops(l):
+        p = []
+        for i in l:
+                p.append(i.replace('"', "").split(','))
+        return p
+
+def make_pop_tuples(c):
+
+        County = namedtuple('County', ['Rank', 'Name'])
+        return list(map((lambda x: County(int(x[0]), x[1])),
+                get_pops(c)))
+
+def make_bracket(b):
+
+        random.shuffle(b)
+        pairs = []
+        for i in range(0, len(b) - 1, 2):
+                pairs.append((b[i], b[i+1]))
+
+        return pairs
+
+
+def ask_pair(p):
+        text = f"""Which has the larger population?
+                1. {p[0].Name}
+                2. {p[1].Name}\n"""
+        guess = int(input(text))
+        answer = 1 if p[0].Rank < p[1].Rank else 2
+
+        return p[guess - 1], guess == answer
+
+def play_a_game(bracket):
+        score = 0
+        game = bracket.copy()
+
+        i = 0
+        while len(game) > 1:
+                next_round = []
+                for p in game:
+                        os.system('clear')
+                        print(f"\nCurrent Score: {score}/{i}\n")
+
+                        n, win = ask_pair(p)
+                        if win: score += 1
+                        next_round.append(n)
+                        i += 1
+
+                game = make_bracket(next_round)
+
+
+if __name__ == '__main__':
+        with open('pops.csv') as f:
+                contents = f.read().splitlines()
+
+        bracket = make_bracket(make_pop_tuples(contents))
+
